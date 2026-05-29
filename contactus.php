9aeb2d5d52affb29e37a2491d9056e8473c9affd
TwinkleMedia <hs0052264@gmail.com>
2026-05-29 14:11:41 +0530

    Title

--- contactus.php
+++ contactus.php
@@ -89,7 +89,8 @@ include "./whatsapp.php"
         <svg class="w-3.5 h-3.5 text-[#7eb3ff]" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
           <path stroke-linecap="round" stroke-linejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0zM15 11a3 3 0 11-6 0 3 3 0 016 0z"/>
         </svg>
-        Maritex Shipping Pvt Ltd , Morabadi , Ranchi , Jharkhand India
+        
+ 
       </span>
     </div>
   </div>
